8dbaf68822100e21c5671381798a9ee374fdf8c8
Jan Macku <jamacku@redhat.com>
2020-01-03 16:14:00 +0100

    Add support for passed parameters and use better place to create initrd (/tmp)

--- etc/kernel/postinst.d/dracut-removal-spel.sh
+++ etc/kernel/postinst.d/dracut-removal-spel.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-NEW_KERNEL="$(rpm -q --last kernel | head -1 | awk '{print $1}')"
+NEW_KERNEL="$1"
+KERNEL_IMAGE="$2"
 NEW_INITRD="nondracut-initramfs-${NEW_KERNEL}.img"
 NEW_TITLE="Fedora ${NEW_KERNEL} dracut FREE"
 
 [ -f /etc/initrd.conf ] && readarray PACKAGES < /etc/initrd.conf
 
-mkdir /root/initrd
+INITRD_DIR=$(mktemp -d) # better would be /var/tmp
 
 dnf -y update
-dnf install --setopt=install_weak_deps=False --installroot=/root/initrd/ --releasever=31 \
+dnf install --setopt=install_weak_deps=False --installroot="$INITRD_DIR" --releasever=31 \
   systemd \
   passwd \
   fedora-release \
@@ -19,13 +20,14 @@ dnf install --setopt=install_weak_deps=False --installroot=/root/initrd/ --relea
   kernel-modules \
   "${PACKAGES[@]}"
 
-touch /root/initrd/etc/initrd-release
-ln -s /lib/systemd/systemd /root/initrd/init
-systemctl --root /root/initrd set-default initrd.target
+touch "${INITRD_DIR}/etc/initrd-release"
+ln -s /lib/systemd/systemd "${INITRD_DIR}/init"
+systemctl --root "$INITRD_DIR" set-default initrd.target
 
-cd /root/inirtd/ || exit 1
+cd "$INITRD_DIR" || exit 1
 find . | cpio -o -c | gzip -9 > "/boot/${NEW_INITRD}"
+rm -rf "$INITRD_DIR"
 
 # generate grup config
-grubby --add-kernel="/boot/vmlinuz-${NEW_KERNEL}" --boot-filesystem="/boot/${NEW_INITRD}" --grup --title="${NEW_TITLE}"
+grubby --add-kernel="$KERNEL_IMAGE" --boot-filesystem="/boot/${NEW_INITRD}" --grup --title="${NEW_TITLE}"
 # grubby --add-kernel="/boot/vmlinuz-5.2.18-100.fc29.x86_64" --initrd="/boot/initramfs-5.2.18-100.fc29.x86_64.img" --grub2 --title="New Initramfs"
